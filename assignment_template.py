95ca1a9953c41990597acc386812d0697f23fae4
lachiegraham <61955647+lachiegraham@users.noreply.github.com>
2021-05-10 11:05:31 +1000

    Add files via upload

---
+++ assignment_template.py
@@ -0,0 +1,57 @@
+"""
+This is the assignment template for the COMP1730/COMP6730 major assignment
+for Semester 1, 2021.
+
+The assignment is due at 9:00am on Monday 24 May.
+
+Please include the student IDs of all members of your group here
+Student Ids:
+"""
+
+
+# Question 1:
+def read_dataset(file):
+    pass
+
+
+# Question 2:
+def minimum_elevation(data_set):
+    pass
+
+
+def maximum_elevation(data_set):
+    pass
+
+
+def average_elevation(data_set):
+    pass
+
+
+# Question 3
+def slope(data_set, x_coordinate, y_coordinate):
+    pass
+
+
+# Question 4
+def surface_area(data_set, x_coordinate, y_coordinate):
+    pass
+
+
+# Question 5:
+def expanded_surface_area(data_set, water_level, x_coordinate, y_coordinate):
+    pass
+
+
+# Question 6:
+def impute_missing_values(data_set):
+    pass
+
+# You'll need to decide what other functions you want for Question 6
+# It should be clear from your code, what we need to do in order to produce the plot(s).
+
+
+# Code in the following if statement will only be executed when this file is run - not when it is imported.
+# If you want to use any of your functions (such as to answer questions) please write the code to
+# do so inside this if statement. We'll cover it in more detail in an upcoming lecture.
+if __name__ == "__main__":
+    pass
---
+++ assignment_tests.py
@@ -0,0 +1,14 @@
+"""
+Test file for the COMP1730/COMP6730 major assignment,
+Semester 1, 2021.
+
+Please write all tests you develop in this file, and submit it
+along with assignment_template.py
+
+Please include the student IDs of all members of your group here
+Student Ids:
+"""
+
+import pytest
+from assignment_template import *  # This will import all the functions from assignment_template.py
+
